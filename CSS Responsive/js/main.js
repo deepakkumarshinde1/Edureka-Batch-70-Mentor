fcc4e16c2419a488e8e40d14148e7c6d9fe2de4d
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-01-04 19:58:39 +0530

    responsive

---
+++ CSS Responsive/js/main.js
@@ -0,0 +1,12 @@
+//https://github.com/RoopKiranReddy/code-check-assignment.git
+
+// API => Application Programming Interface
+// handel a specific operations
+// DOM, Promise , Async , Async/Await , Storage API, XHR , Fetch => Only on browser in javascript
+
+// Client ==> Request ==> Server
+// Client <== Response <== Server
+// HTTP => REST (Representational State Transfer) API
+
+// to handel // create and consume => REST API => XHR or Fetch API
+// XHR & FETCH are async(non-block) in nature

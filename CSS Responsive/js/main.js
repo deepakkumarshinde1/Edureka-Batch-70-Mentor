85d1c8da2315943a15ca7296da8731d7687227e7
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-01-06 20:02:22 +0530

    callback

--- CSS Responsive/js/main.js
+++ CSS Responsive/js/main.js
@@ -1,4 +1,4 @@
-//https://github.com/RoopKiranReddy/code-check-assignment.git
+//
 
 // API => Application Programming Interface
 // handel a specific operations
@@ -10,3 +10,31 @@
 
 // to handel // create and consume => REST API => XHR or Fetch API
 // XHR & FETCH are async(non-block) in nature
+
+// in build js function
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(getData);
+} else {
+  x.innerHTML = "Geolocation is not supported by this browser.";
+}
+
+function getCurrentPosition(callback) {
+  let coords = {
+    accuracy: 2019.2207213381607,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    latitude: 19.9681117,
+    longitude: 73.8066048,
+    speed: null,
+  };
+  callback(coords);
+}
+
+getCurrentPosition(getData);
+
+function getData(position) {
+  console.log(position);
+}
+// function call and in that function call we have defined a function
